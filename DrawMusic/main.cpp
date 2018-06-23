b204ca5321ef6d2cff449ff9f1d9f0e9fe589727
Yang Liu <lywander@gmail.com>
2018-06-23 11:57:15 -0700

    init

---
+++ DrawMusic/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DrawMusic
+//
+//  Created by Yang Liu on 6/23/18.
+//  Copyright © 2018 PixelsRhythm. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
